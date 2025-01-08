953262abf25ce88e13209944a5d260b54ab859f3
Lise <165786617+Noobliz@users.noreply.github.com>
2025-01-08 09:56:43 +0100

    Update valid_map.c

--- valid_map.c
+++ valid_map.c
@@ -10,52 +10,49 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-typedef struct s_map
-{
-	int				value;
-	struct s_map	*x;
-	struct s_map	*y;
-}					t_map;
+#include <stdio.h>
+#include <stdlib.h>
 
-int	surrounded_by_walls(t_map *map)
-{
-	t_map *tmp;
-	t_map *right_side;
-	t_map *end;
-	tmp = map;
-	// verif de la premiere ligne
-	while (tmp && tmp->x)
-	{
-		if (tmp->x->value != 1)
-		{
+typedef struct s_map {
+	int				value;  
+	struct s_map	*x;     //à droite
+	struct s_map	*y;     // en bas
+} t_map;
+
+int	surrounded_by_walls(t_map *map) {
+	t_map *tmp = map;
+	t_map *right_side = NULL;
+
+	// Vérification de la première ligne (haut)
+	while (tmp) {
+		if (tmp->value != 1) 
 			return (0);
-		}
+		if (!tmp->x) 
+			right_side = tmp;
 		tmp = tmp->x;
 	}
-	right_side = tmp;
-	tmp = map->y;
 
+	// Vérification de la dernière ligne (bas)
+	tmp = map;
 	while (tmp->y)
 		tmp = tmp->y;
-	// verif de la derniere ligne
-	while (tmp && tmp->x)
-	{
-		if (tmp->x->value != 1)
-		{
+
+	while (tmp) {
+		if (tmp->value != 1)
 			return (0);
-		}
 		tmp = tmp->x;
 	}
-	tmp = map;
-	// verif cote gauche et droit
-	while ((tmp && tmp->y) || (right_side && right_side->y))
-	{
-		if (tmp->y->value != 1 || right_side->y->value != 1)
+
+	// Vérification des côtés gauche et droit
+	t_map *left_side = map; // Début du côté gauche
+	tmp = right_side;       // Début du côté droit
+
+	while (left_side && tmp) {
+		if (left_side->value != 1 || tmp->value != 1) 
 			return (0);
-		if (tmp)
-			tmp = tmp->y;
-		if (right_side)
-			right_side = right_side->y;
+		left_side = left_side->y;
+		tmp = tmp->y;          
 	}
+
 	return (1);
 }
