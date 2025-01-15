a87b693c19c7037c18c352683f727359ffb8f050
Lise <165786617+Noobliz@users.noreply.github.com>
2025-01-15 10:01:34 +0100

    Update handle_window.c

--- handle_window.c
+++ handle_window.c
@@ -24,19 +24,29 @@ int	handle_input(int keysym, t_data *data)
 		mlx_destroy_window(data->mlx, data->window);
 	return (0);
 }
+void	destroy_images(t_img *img, void *mlx)
+{
+	if (img->img_wall)
+		mlx_destroy_image(mlx, img->img_wall);
+	if (img->img_floor)
+		mlx_destroy_image(mlx, img->img_floor);
+	if (img->img_lantern)
+		mlx_destroy_image(mlx, img->img_lantern);
+	if (img->img_char)
+		mlx_destroy_image(mlx, img->img_char);
+	if (img->img_coll)
+		mlx_destroy_image(mlx, img->img_coll);
+	if (img->img_exit)
+		mlx_destroy_image(mlx, img->img_exit);
+}
+
 int	handle_keypress(int keysym, t_data *data)
 {
 	if (keysym == XK_Escape)
 	{
-		// free_map(map);
-		// mlx_destroy_image(data->mlx, img->img_char);
-		// mlx_destroy_image(data->mlx, img->img_coll);
-		// mlx_destroy_image(data->mlx, img->img_exit);
-		// mlx_destroy_image(data->mlx, img->img_floor);
-		// mlx_destroy_image(data->mlx, img->img_wall);
-		// mlx_destroy_image(data->mlx, img->img_lantern);
-		// mlx_destroy_window(data->mlx, data->window);
-		// free(map);
+		destroy_images(data.img, data.mlx)
+		mlx_destroy_window(data->mlx, data->window);
+		free_map(map);
 		mlx_destroy_display(data->mlx);
 		free(data->mlx);
 		exit(0);
